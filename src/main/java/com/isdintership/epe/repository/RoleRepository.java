4508657878b24982993abe5ad0c921cf45c32650
ExxaD <max7gri@gmail.com>
2021-11-10 14:39:46 +0200

    repositories annotations

--- src/main/java/com/isdintership/epe/repository/RoleRepository.java
+++ src/main/java/com/isdintership/epe/repository/RoleRepository.java
@@ -2,8 +2,11 @@ package com.isdintership.epe.repository;
 
 import com.isdintership.epe.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
     Role findByRole(String name);
 
 }
